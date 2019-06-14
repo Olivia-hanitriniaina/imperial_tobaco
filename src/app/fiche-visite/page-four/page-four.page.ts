3f60d05231b6ad0a7c7dfbe1fc5cbc0d34919d9d
ntsouh_rmh <ntsouhwork@gmail.com>
2019-06-14 16:46:32 -0700

    les 7 pages

--- src/app/fiche-visite/page-four/page-four.page.ts
+++ src/app/fiche-visite/page-four/page-four.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-four',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-four.page.scss'],
 })
 export class PageFourPage implements OnInit {
+	items: { id: number; label: string; } [];
+	cols: any[];
 
-  constructor() { }
+	constructor(private router : Router) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.items = [
+	      {id : 1 , label:'NOUVEAU'},
+	      {id : 2 , label:'OUVERTE'},
+	      {id : 3 , label:'CLÔTURÉE'},
+	    ];
 
+	    // this.carService.getCarsSmall().then(cars => this.cars = cars);
+
+	    this.cols = [
+	        { field: 'vin', header: 'Vin' },
+	        {field: 'year', header: 'Year' },
+	        { field: 'brand', header: 'Brand' },
+	        { field: 'color', header: 'Color' }
+	    ];
+	}
+
+	page_three(){
+	    this.router.navigate(['page-three']) ;
+  	}
+
+  	page_five(){
+	    this.router.navigate(['page-five']) ;
+  	}
 }
