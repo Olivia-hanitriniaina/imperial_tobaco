72833fd877cb4e0f7bea33c311c1208cd93dc072
ntsouh_rmh <ntsouhwork@gmail.com>
2019-06-21 16:59:42 -0700

    fiche visite complete

--- src/app/fiche-visite/page-seven/page-seven.page.ts
+++ src/app/fiche-visite/page-seven/page-seven.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-seven',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageSevenPage implements OnInit {
 
-  constructor() { }
+ 	items: { id: number; label: string; } [];
+	cols: any[];
 
-  ngOnInit() {
-  }
+	constructor(private router : Router) { }
+
+	ngOnInit() {
+		this.items = [
+	      {id : 1 , label:'NOUVEAU'},
+	      {id : 2 , label:'OUVERTE'},
+	      {id : 3 , label:'CLÔTURÉE'},
+	    ];
+
+	    // this.carService.getCarsSmall().then(cars => this.cars = cars);
+
+	    this.cols = [
+	        { field: 'vin', header: 'Vin' },
+	        {field: 'year', header: 'Year' },
+	        { field: 'brand', header: 'Brand' },
+	        { field: 'color', header: 'Color' }
+	    ];
+	}
+
+	page_six(){
+	    this.router.navigate(['page-six']) ;
+	}
 
 }
